2e5161e2869ca8dc2d764d0e951c6d21e72ff7a3
ChenCoin <chencoin.cn@gmail.com>
2024-07-10 20:42:52 +0800

    add markdown index

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+import 'package:markdown_widget/markdown_widget.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+/// dependency
+///   http: ^1.2.1
+///   markdown_widget: ^2.3.2+6
+/// Markdown_Widget的图片处理
+/// 图片地址不是http开头的，转由flutter的系统Widget处理：Image.asset
 
 void main() {
   runApp(const MyApp());
@@ -7,28 +17,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -39,15 +33,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +40,94 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ConstrainedBox(
+        constraints: const BoxConstraints.expand(),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              width: 720,
+              margin: const EdgeInsets.all(8),
+              decoration: createBoxDecoration(),
+              child: buildFuture(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  BoxDecoration createBoxDecoration() {
+    return BoxDecoration(
+      border: Border.all(
+        color: Colors.grey.withOpacity(0.2),
+        width: 1,
+      ),
+      color: Colors.white,
+      boxShadow: [
+        BoxShadow(
+          blurRadius: 10,
+          spreadRadius: 0.1,
+          color: Colors.grey.withOpacity(0.2),
+        ),
+      ],
+      borderRadius: BorderRadius.circular(10),
+    );
+  }
+
+  Widget buildFuture() {
+    final url = Uri.parse("${Uri.base.origin}/page/index.md");
+    final config = MarkdownConfig(configs: [
+      LinkConfig(onTap: (url) {
+        launchUrl(Uri.parse(url));
+      }),
+      ImgConfig(builder: (String imgUrl, Map<String, String> attributes) {
+        final isNetImage =
+            imgUrl.startsWith('http://') || imgUrl.startsWith('https://');
+        final img = isNetImage ? imgUrl : "${Uri.base.origin}/page/$imgUrl";
+        return Image.network(img, fit: BoxFit.cover, errorBuilder: (c, e, _) {
+          return const Icon(Icons.broken_image, color: Colors.grey);
+        });
+      })
+    ]);
+    return FutureBuilder<String>(
+      future: mockNetworkData(url),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Text("Error: ${snapshot.error}");
+          }
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+            child: MarkdownBlock(
+              data: snapshot.data,
+              config: config,
+            ),
+          );
+        }
+        return const CircularProgressIndicator();
+      },
+    );
+  }
+
+  Future<String> mockNetworkData(Uri url) async {
+    try {
+      final headers = <String, String>{'charset': 'utf-8'};
+      final response = await http.get(url, headers: headers);
+      debugPrint('Response status: ${response.statusCode}');
+
+      // 方法1：手动解析
+      // final content = utf8.decode(response.bodyBytes);
+      // debugPrint('Response body: $content');
+
+      // 方法2：在content-type设置charset
+      // 直接设置charset是无效的，如 response.headers['charset'] = 'utf-8';
+      response.headers['content-type'] = 'text/plain; charset=utf-8';
+      return Future(() => response.body);
+    } catch (exception) {
+      return Future(() => "404");
+    }
   }
 }
